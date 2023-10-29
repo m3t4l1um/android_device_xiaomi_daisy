cf5c2e6103c9f184db5fc77a2b186113427115f5
Giovanni Ricca <giovanniricca@proton.me>
2023-10-29 17:24:19 +0100

    daisy: Don't let patchelf corrupt the lib
    Change-Id: I268616bdde695c14121f4a337d1fb1c2897ceacd

--- device.mk
+++ device.mk
@@ -72,7 +72,7 @@ endif
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/v32hidlbase.so
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/daisy/daisy-vendor.mk)
